17dff6b469d30e321f6d5709effa7fe9bb47a263
gyhcom <59407250+gyhcom@users.noreply.github.com>
2026-03-24 22:05:32 +0900

    Start Resume Builder

---
+++ src/main/java/gyh/resumebuilderapi/document/User.java
@@ -0,0 +1,15 @@
+package gyh.resumebuilderapi.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collation = "users")
+public class User {
+}
