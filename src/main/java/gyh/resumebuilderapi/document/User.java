0b94a4927acb58ce87d402f5ae955db6de8a05bd
gyhcom <59407250+gyhcom@users.noreply.github.com>
2026-03-25 21:54:20 +0900

    Feat: Add user model and registration functionality with MongoDB auditing

--- src/main/java/gyh/resumebuilderapi/document/User.java
+++ src/main/java/gyh/resumebuilderapi/document/User.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -12,4 +16,19 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Builder
 @Document(collation = "users")
 public class User {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String profileImageUrl;
+    private String subscriptionPlan = "basic";
+    private boolean emailVerified = false;
+    private String verificationToken;
+    private LocalDateTime verificationExpires;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
 }
